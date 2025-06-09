53028c95dbdb59b04f52670452fbbb069bcd8308
Eladio Caritos <eladio@caritos.com>
2025-06-09 10:02:23 -0400

    add tag cloud to index page

---
+++ _plugins/tag_generator.rb
@@ -0,0 +1,24 @@
+module Jekyll
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      site.tags.each do |tag, posts|
+        site.pages << TagPage.new(site, site.source, tag, posts)
+      end
+    end
+  end
+
+  class TagPage < Page
+    def initialize(site, base, tag, posts)
+      @site = site
+      @base = base
+      @dir  = File.join('tags', tag)
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['tag'] = tag
+      self.data['title'] = "Posts tagged with '#{tag}'"
+    end
+  end
+end 
